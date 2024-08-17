02651e699bf088957c8dabe97a1af47a961f154a
hyh449 <homework88888@naver.com>
2024-08-17 20:24:20 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 // script.js 파일 내용
 function calculateLungCancerRisk(age, smokingYears, dailyCigarettes) {
-    let risk = (age * 0.1) + (smokingYears * 0.2) + (dailyCigarettes * 0.3);
-    return risk;
+    let risk = Logodds = (0.0778868 * (age - 62)) + race - (0.0812744 * (education - 4)) - (0.0274194 * (bodymassindex - 27)) 
+    + copd + hxca + fmhca + smoker - (1.822606 * ((10 / dailycigarettes) - 0.4021541613)) + (0.0317321 * (smokingyears - 27)) - 
+    (0.0308572 * (yearssincequittingsmoking - 10)) - 4.532506
+    Risk = 100 * (e(Logodds) / (1 + e(Logodds)));
+    return Risk;
 }
 
 document.addEventListener('DOMContentLoaded', function() {
